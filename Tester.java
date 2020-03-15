effce76e56b6208f1343eff9098e473390e04294
Eric Truong <pengu@erics-mbp-2.lan>
2020-03-15 11:48:59 -0700

    Added comments and sales file

--- Tester.java
+++ Tester.java
@@ -5,14 +5,20 @@ import java.util.ArrayList;
 public class Tester {
     public static void main (String[] args){
 
+        // Create arraylist to hold all data
         ArrayList<Data> dataStrings = new ArrayList<>();
 
+        // reads the file
         readFile(dataStrings);
 
+
+        // makes sure it's read correctly
         for (int i = 0; i < dataStrings.size(); i++){
 
             System.out.println(dataStrings.get(i));
         }
+
+        // if user wants to add data to the file, call the create function first and then the write function
     }
 
     public static void readFile(ArrayList<Data> dataStrings){
@@ -23,9 +29,12 @@ public class Tester {
 
             do {
 
+                // Reads each line and puts it in it's respective category
                 String line = read.nextLine();
                 String [] tokens = line.split(";");
                 Data readLine = new Data(tokens[0], tokens[1], tokens[2], tokens[3]);
+
+                // Adds it to the arraylist
                 dataStrings.add(readLine);
 
             }
@@ -48,12 +57,14 @@ public class Tester {
 
         boolean write = true;
 
+        // Allows person to write their own data
         while (write){
 
             System.out.print("Would you like to write data? Y/N: ");
 
             String decision = scan.nextLine();
 
+            // Creates data from user input
             if (decision.equals("Y") || decision.equals("y")){
 
                 System.out.print("Name: ");
@@ -72,6 +83,7 @@ public class Tester {
 
                 String date = scan.nextLine();
 
+                // Adds it to the existing arraylist
                 dataStrings.add(new Data(name, service, amount, date));
             }
 
@@ -90,6 +102,7 @@ public class Tester {
 
         try {
 
+            // Writes all the data from the arraylist into the file
             PrintWriter write = new PrintWriter("sales.txt");
 
             for (int i = 0; i < dataStrings.size(); i++){
